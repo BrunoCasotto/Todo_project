816bd0778929c7dced522c1618fca5ecaf8b0886
Bruno Casotto <casottoalves@hotmail.com>
2019-11-21 00:06:16 -0300

    login validate done

--- backend/app/controllers/auth.js
+++ backend/app/controllers/auth.js
@@ -1,6 +1,7 @@
 const express = require('express')
 const User = require('../db/models/user')
 const router = express.Router()
+const bcryptjs = require('bcryptjs')
 
 //simple method to create new users
 router.post('/register', async (req, res) => {
@@ -12,6 +13,24 @@ router.post('/register', async (req, res) => {
   } catch (err) {
     console.error('AuthController::register', { err })
     res.status(400).send({ error: 'Registration failed '})
+  }
+})
+
+//simple method to create new users
+router.post('/authenticate', async (req, res) => {
+  const { email, password } = req.body
+  try {
+    const user = await User.findOne({ email }).select('+password')
+
+    if(! await bcryptjs.compare(password, user.password)) {
+      return res.status(401).send({ error: 'Invalid password'})
+    }
+
+    user.password = undefined
+    return res.send({ user })
+  } catch (err) {
+    console.error('AuthController::register', { err })
+    res.status(400).send({ error: 'Authentication failed'})
   }
 })
 
