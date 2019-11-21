95e766963f745f63d790545a789b61618cebc7d0
Bruno Casotto <casottoalves@hotmail.com>
2019-11-20 23:52:11 -0300

    Update db default name

--- backend/app/db/index.js
+++ backend/app/db/index.js
@@ -3,7 +3,7 @@ const mongoose = require('mongoose')
 const dbUser = process.env.DB_USER || 'root'
 const dbPassword = process.env.DB_PASSWORD || 'root'
 const host = process.env.DB_HOST || 'localhost:27017'
-const database = process.env.DB_DATABASE || 'admin'
+const database = process.env.DB_DATABASE || 'todo_db'
 
 const mongooseOptions = {
   useNewUrlParser: true,
