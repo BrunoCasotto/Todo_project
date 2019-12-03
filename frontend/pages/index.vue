181d5fa6a5ee98b389c8f7bd37489b05bcb9d110
Bruno Casotto <casottoalves@hotmail.com>
2019-12-03 13:12:49 -0300

    Wellcome dashboard created

--- frontend/pages/index.vue
+++ frontend/pages/index.vue
@@ -1,19 +1,46 @@
 <template>
   <div class="home-page">
     <div class="home-page__content text-center">
-      <h4 class="mb-4">
-        Bem vindo ao sistema
+      <h4 class="mb-4" v-if="loggedIn">
+        Bem vindo ao sistema, {{ firstName }}
       </h4>
-      <nuxt-link to="/login">
-        Entrar
+      <template v-else>
+        <h4 class="mb-4">
+          Olá,
+        </h4>
+        <p><em>Entre ou cadastre-se para começar</em></p>
+      </template>
+
+      <nuxt-link to="/Dashboard" v-if="loggedIn" >
+        Ir para tarefas
       </nuxt-link>
+      <template v-else>
+        <nuxt-link to="/login">
+          Entrar
+        </nuxt-link> |
+        <nuxt-link to="/signup">
+          Não sou cadastrado
+        </nuxt-link>
+
+      </template>
     </div>
   </div>
 </template>
 
 <script>
+import { mapState } from 'vuex'
 export default {
-  auth: false
+  auth: false,
+  computed: {
+    ...mapState('auth', ['user', 'loggedIn']),
+    firstName() {
+      if(this.user && this.user.name) {
+        return this.user.name.split(' ')[0]
+      } else {
+        return ''
+      }
+    }
+  }
 }
 </script>
 
@@ -30,6 +57,11 @@ export default {
      border-radius: 8px;
      max-width: 300px;
      margin: 0 auto;
+     width: 100%;
+
+     @media screen and(min-width: 730px) {
+       min-width: 700px;
+     }
    }
  }
 </style>
